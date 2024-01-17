3aa216c9d26eb52757867f88117c6392b0518725
cynthiacc512 <cynthiacc512@gmail.com>
2024-01-17 11:26:02 +0700

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import logo from './404.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="maintenance-page">
+            <h1 class="nl_open-sans">OOPS, Ada yang Salah!</h1>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Sepertinya kita yang salah. Halaman ini sedang tidak bisa diakses.</p>
+            <p>Kamu bisa balik ke home dengan cara klik 
+            <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+             link ini
+          </a>
+          </p>
+          </div>
       </header>
+
     </div>
   );
 }
